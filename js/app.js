f5049197e4383c98568509e57a3841de7e1cc773
vicGeo <vicsanig@alumni.upves>
2021-02-15 23:03:10 +0100

    Probamos el evento onclick por consola

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//Seleccionamos elementos del DOM
+const buttonElement = document.querySelector('#search');
+const inputElement = document.querySelector('#inputValue');
+
+//Funcion onclick
+
+buttonElement.onclick = () => {
+    console.log('Click button!');
+};
