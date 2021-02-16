7c17f0567caa5fc6b9ac99202eb189e1021e545a
vicGeo <vicsanig@alumni.upves>
2021-02-16 18:27:18 +0100

    Creamos las funciones para pintar las pelis

--- js/app.js
+++ js/app.js
@@ -8,7 +8,39 @@ const url = 'https://api.themoviedb.org/3/search/movie?api_key=ed8717a7e8e8309c8
 const buttonElement = document.querySelector('#search');
 const inputElement = document.querySelector('#inputValue');
 
-//Funcion onclick
+/*
+            <section class="section-movie">
+                <img src="https://image.tmdb.org/t/p/w500/oZRlkaeMBwj6SByLNkBv8m8r6JU.jpg" movie-id="1">
+                <img src="https://image.tmdb.org/t/p/w500/245tgZ3mTHWs0NRiPSwPjKobKHf.jpg" movie-id="2">
+            </section>
+*/
+
+//Función para pintar las pelis en el DOM
+
+const movieSection = (movieRender) => {
+    return movieRender.map((movie) => {
+        return `
+                <img src=https://image.tmdb.org/t/p/w500/${movie.poster_path} movie-id=${movie.id}/>
+            `;
+    });
+};
+
+
+const createMovie = (movieRender) => {
+    const movieElement = document.createElement('div');
+    movieElement.setAttribute('class', 'movie');
+
+    const movieTemplate = `
+        <section class="section-movie">
+            ${movieSection(movieRender)}
+        </section>
+    `;
+
+    movieElement.innerHTML = movieTemplate;
+    return movieElement;
+}
+
+//Función onclick
 
 buttonElement.onclick = e => {
     e.preventDefault();
@@ -19,6 +51,7 @@ buttonElement.onclick = e => {
     fetch(newUrl)
         .then((res) => res.json())
         .then((data) => {
+            // data.results []
             console.log('Data: ', data);
         })
         .catch((error) => {
